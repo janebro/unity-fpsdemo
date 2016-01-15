8005ac6a1f49b1fb5be47571a613dcbf2981bd36
Ernesto Janebro <ernesto.camurca@gmail.com>
2016-01-15 14:08:46 -0300

    Ajustes

--- Assets/Scripts/Weapon/AimScope.cs
+++ Assets/Scripts/Weapon/AimScope.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class AimScope : MonoBehaviour {
 
 	public GameObject[] weapons;
+	public GameObject playerCAM;
 
 	void Update ()
 	{
@@ -11,28 +12,30 @@ public class AimScope : MonoBehaviour {
 		{
 			switch (weapons[0].name) 
 			{
-				case "acr_rifle":
-					transform.localPosition = new Vector3 (-0.224f, 0f, 0f);
-					break;
-				case "Assault Rifle":
-					transform.localPosition = new Vector3 (0.1f, 0f, 0f);
-					break;
-				default:
-					break;
+			case "acr_rifle":
+				transform.localPosition = new Vector3 (-0.224f, 0f, 0f);
+				break;
+			case "Assault Rifle":
+				transform.localPosition = new Vector3 (0.1f, 0f, 0f);
+				playerCAM.GetComponent<Camera> ().fieldOfView = 30f;
+				break;
+			default:
+				break;
 			}
 		} 
 		else 
 		{
 			switch (weapons[0].name) 
 			{
-				case "acr_rifle":
+			case "acr_rifle":
 				transform.localPosition = Vector3.zero;
-					break;
-				case "Assault Rifle":
+				break;
+			case "Assault Rifle":
 				transform.localPosition = new Vector3 (0.2f, 0f, 0f);
-					break;
-				default:
-					break;
+				playerCAM.GetComponent<Camera> ().ResetFieldOfView ();
+				break;
+			default:
+				break;
 			}
 		}
 	}
