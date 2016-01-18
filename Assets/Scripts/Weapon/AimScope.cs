2ded0760a84a9f57207bdc256e43b301a2f06e9a
Eduardo Bruno <eduardobruno@hotmail.co.uk>
2016-01-18 01:42:38 -0200

    Dei uma modificada no AimScope pra deixar mais simples e ser único pra cada arma. Pra cada prefab de arma a gente só precisa dizer o Vector3 da posição de quando a arma estiver com o zoom e o FOV necessário. Deixei o código antigo lá comentado caso queira voltar ao anterior.

--- Assets/Scripts/Weapon/AimScope.cs
+++ Assets/Scripts/Weapon/AimScope.cs
@@ -6,37 +6,75 @@ public class AimScope : MonoBehaviour {
 	public GameObject[] weapons;
 	public GameObject playerCAM;
 
+    public Vector3 weaponScopePosition;
+    public float zoomAmount;
+
+    private Vector3 weaponDefaultPosition;
+    private float defaultFOV;
+    private Camera cam;
+
+    void Start()
+    {
+        cam = playerCAM.GetComponent<Camera>();
+        weaponDefaultPosition = transform.localPosition;
+        defaultFOV = cam.fieldOfView;
+    }
+
 	void Update ()
 	{
-		if(Input.GetMouseButton(1)) 
-		{
-			switch (weapons[0].name) 
-			{
-			case "acr_rifle":
-				transform.localPosition = new Vector3 (-0.224f, 0f, 0f);
-				break;
-			case "Assault Rifle":
-				transform.localPosition = new Vector3 (0.1f, 0f, 0f);
-				playerCAM.GetComponent<Camera> ().fieldOfView = 30f;
-				break;
-			default:
-				break;
-			}
-		} 
-		else 
-		{
-			switch (weapons[0].name) 
-			{
-			case "acr_rifle":
-				transform.localPosition = Vector3.zero;
-				break;
-			case "Assault Rifle":
-				transform.localPosition = new Vector3 (0.2f, 0f, 0f);
-				playerCAM.GetComponent<Camera> ().ResetFieldOfView ();
-				break;
-			default:
-				break;
-			}
-		}
+
+        /*  if (Input.GetMouseButton(0))
+            Botão esquerdo do mouse.
+        
+            if (Input.GetMouseButton(1))
+            Botão direito do mouse.
+        
+            if (Input.GetMouseButton(2))
+            Botão do meio do mouse.
+         * 
+         */
+
+        if (Input.GetMouseButton(1))
+        {
+            transform.localPosition = weaponScopePosition;
+            cam.fieldOfView = zoomAmount;
+        }
+        else
+        {
+            transform.localPosition = weaponDefaultPosition;
+            cam.fieldOfView = defaultFOV;
+        }
+
+        //if(Input.GetMouseButton(1))//Apertar botão direito.
+        //{
+        //    switch (weapons[0].name) 
+        //    {
+        //    case "acr_rifle":
+        //        transform.localPosition = new Vector3 (-0.224f, 0f, 0f);
+        //        break;
+        //    case "Assault Rifle":
+        //        transform.localPosition = new Vector3(-0.098f, 0.014f, 0f);
+        //        playerCAM.GetComponent<Camera>().fieldOfView = 30f;
+        //        break;
+        //    default:
+        //        break;
+        //    }
+        //} 
+        //else //Soltar botão direito;
+        //{
+        //    switch (weapons[0].name) 
+        //    {
+        //    case "acr_rifle":
+        //        transform.localPosition = Vector3.zero;
+        //        break;
+        //    case "Assault Rifle":
+        //        //transform.localPosition = new Vector3 (0.2f, 0f, 0f);
+        //        transform.localPosition = weaponDefaultPosition;
+        //        playerCAM.GetComponent<Camera>().fieldOfView = defaultFOV;
+        //        break;
+        //    default:
+        //        break;
+        //    }
+        //}
 	}
 }
