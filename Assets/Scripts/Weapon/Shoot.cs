fc29897b3a1fc9b215d9f9023bd19f39a3bb42ba
Eduardo Bruno <eduardobruno@hotmail.co.uk>
2016-01-18 03:24:02 -0200

    Refiz o script de animações das armas pra C#, antes estava em JS. O script Shoot agora pode ser usado em cada arma. Ele tem uma Mask agora que da pra selecionar quais layers você quer que o raycast use. No próximo commit vou adicionar munição.

--- Assets/Scripts/Weapon/Shoot.cs
+++ Assets/Scripts/Weapon/Shoot.cs
@@ -4,21 +4,26 @@ using System.Collections;
 public class Shoot : MonoBehaviour {
 
     //public GameObject bullet;
+    public AudioClip fireSound;
 	public GameObject bulletHole;
+    public WeaponAnimationsScript animationsRef;
+
+    public GameObject muzzleSpot;
+    public LayerMask mask; //Uma layermask. Tudo que tiver selecionado no Inspector vai ser ignorado.
 
 	void Update () {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && animationsRef.reloading == false && animationsRef.drawWeapon == false)
         {
             //Instantiate(bullet, transform.position, transform.rotation);
-            GetComponent<AudioSource>().Play();
-			GameObject.Find ("MuzzleSpot").GetComponent<ParticleSystem> ().Play ();
+            GetComponent<AudioSource>().PlayOneShot(fireSound);
+			muzzleSpot.GetComponent<ParticleSystem>().Play ();
 
 			RaycastHit hit;
-			Ray ray = new Ray (transform.position, transform.forward);
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f)); //Cria o raycast do centro da tela.
 
-			if (Physics.Raycast(ray, out hit, 100f)) 
+            if (Physics.Raycast(ray, out hit, 100f, mask))
 			{
-				Instantiate (bulletHole, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
+               Instantiate (bulletHole, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
 			}
         }
 	}
---
+++ Assets/Scripts/Weapon/WeaponAnimationsScript.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponAnimationsScript : MonoBehaviour {
+
+    public string drawAnim, fireLeftAnim, reloadAnim;
+    public GameObject animationGO;
+    public AudioClip drawClip, reloadClip;
+
+    public bool drawWeapon, reloading;
+
+	// Use this for initialization
+	void Start () {
+        drawWeapon = reloading = false;
+        DrawWeapon();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Fire1") && reloading == false && drawWeapon == false)
+        {
+            Fire();
+        }
+
+        if (Input.GetKeyDown("r") && reloading == false && drawWeapon == false)
+        {
+            StartCoroutine(Reloading());
+        }
+
+        if (Input.GetKeyDown("1") && reloading == false)
+        {
+            StartCoroutine("DrawWeapon");
+        }      
+	}
+
+
+    private void Fire()
+    {
+        //animationGO.GetComponent.<Animation>().CrossFadeQueued(fireLeftAnim, 0.08, QueueMode.PlayNow);
+        animationGO.GetComponent<Animation>().CrossFadeQueued(fireLeftAnim, 0.08f, QueueMode.PlayNow); //Faz um 'crossfade' da última animação com a atual que queremos tocar.
+    }
+
+    private IEnumerator Reloading()
+    {
+        if (!reloading)
+        {
+            GetComponent<AudioSource>().clip = reloadClip;
+            GetComponent<AudioSource>().Play();
+            animationGO.GetComponent<Animation>().Play(reloadAnim);
+            reloading = true;
+            yield return new WaitForSeconds(3.8f);
+            reloading = false;
+        }
+        
+    }
+
+    private IEnumerator DrawWeapon()
+    {
+    	GetComponent<AudioSource>().clip = drawClip;
+       	GetComponent<AudioSource>().Play();
+        animationGO.GetComponent<Animation>().Play(drawAnim);
+        drawWeapon = true;
+        yield return new WaitForSeconds(0.6f);
+        drawWeapon = false;
+    }
+}
+
+
+/*
+ * var drawAnim : String = "Draw";
+var fireLeftAnim : String = "Fire";
+var reloadAnim : String = "Reload";
+var animationGO : GameObject;
+var drawClip : AudioClip;
+var reloadClip : AudioClip;
+  
+private var drawWeapon : boolean = false;
+private var reloading : boolean = false;
+
+function Start (){
+    DrawWeapon();
+}
+ 
+function Update (){
+ 
+    if(Input.GetButtonDown ("Fire1") && reloading == false && drawWeapon == false){
+        Fire();
+        }
+       
+        if (Input.GetKeyDown ("r") && reloading == false && drawWeapon == false){
+    Reloading();
+        }
+       
+        if (Input.GetKeyDown ("1") && reloading == false){
+        DrawWeapon();
+        }      
+}
+ 
+function Fire(){
+    animationGO.GetComponent.<Animation>().CrossFadeQueued(fireLeftAnim, 0.08, QueueMode.PlayNow);
+}
+ 
+function DrawWeapon() {
+  if(drawWeapon)
+    return;
+        GetComponent.<AudioSource>().clip = drawClip;
+        GetComponent.<AudioSource>().Play();
+        animationGO.GetComponent.<Animation>().Play(drawAnim);
+        drawWeapon = true;
+        yield WaitForSeconds(0.6);
+        drawWeapon = false;
+       
+}
+ 
+function Reloading(){
+     if(reloading) 
+     return;
+        GetComponent.<AudioSource>().clip = reloadClip;
+        GetComponent.<AudioSource>().Play();
+        animationGO.GetComponent.<Animation>().Play(reloadAnim);
+        reloading = true;
+        yield WaitForSeconds(2.0);
+        reloading = false;
+}
+ * 
+ * 
+*/
