7e0bae1434a3c2db585fd182513cc92e76e21cbf
Eduardo Bruno <eduardobruno@hotmail.co.uk>
2016-01-19 17:15:04 -0200

    Adicionado um novo Enemy: Green Spider. Criei as states machines das animações dela com um lance legal de escolher uma animação random entre 3 animações de ataque. Criei o prefab dela e uns scripts pra testar as animações. Criei uma cena nova também pra começar a implementar a parte de IA dos bonecos e a questão de fazer os spawns deles. Não mexi nisso ainda porque tava ajeitando essa Green Spider.

---
+++ Assets/Scripts/Enemy/RandomIntBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomIntBehaviour : StateMachineBehaviour {
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state. 
+    //Essa função é chamada no primeiro frame do estado sendo tocado.
+	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks.
+    //Essa função é chamada a cada chamada de Update (MonoBehaviour) do objeto em que esse animator está adicionado.
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //Essa função é chamada no último frame de uma transição para outro estado.
+	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    //Essa função é chamada logo depois da função Animator.OnAnimationMove(). 
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+    //Essa função é chamada logo depois da função Animator.OnAnimationMove().
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //base.OnStateEnter(animator, stateInfo, layerIndex);
+        animator.SetInteger("AttackRandom", Random.Range(0, 3));
+        //Debug.Log(animator.)
+    }
+
+}
---
+++ Assets/Scripts/Enemy/SpiderAnimationsTest.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderAnimationsTest : MonoBehaviour {
+
+    public Animator _anim;
+
+    void Start()
+    {
+        if (_anim == null)
+        {
+            _anim = GetComponent<Animator>();
+        }
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            _anim.SetBool("Moving", true);
+        }
+        else
+        {
+            _anim.SetBool("Moving", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            _anim.SetTrigger("Attacking");
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            _anim.SetTrigger("Dead");
+        }
+        
+    }
+}
