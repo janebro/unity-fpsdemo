b7591d649350cfea3ebbdb6e134ce46d22354cbb
Ernesto Janebro <ernesto.camurca@gmail.com>
2016-01-22 15:34:21 -0300

    Menu WIP

---
+++ Assets/Scripts/IA/NavMeshAgentTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMeshAgentTest : MonoBehaviour {
+
+
+    public Transform goal;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+        agent.speed = 10f;
+    }
+
+    void Update()
+    {
+        agent.destination = goal.position;
+    }
+}
