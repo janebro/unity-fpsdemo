df7a3c3ee673804e25a699e50cf0cb3070508e92
Eduardo Bruno <eduardobruno@hotmail.co.uk>
2016-01-22 01:22:48 -0200

    Feito script de animação integrado com o navmesh das aranhas.

--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -10,18 +10,25 @@ public class EnemyStats : MonoBehaviour {
     public GroundChecker groundChk;
 
     private NavMeshAgent agent; //Referência para o componente NavMeshAgent.
+    private Rigidbody rBody;
 
     //NavMeshParameters
+    [Header("NavMesh Parameters")] //No Inspector irá aparecer em negrito escrito isso antes das variáveis abaixo.
     public float radius;
     public float baseOffset;
     public float angularSpeed;
+    public float stoppingDistance;
 
     // Use this for initialization
     void Start()
     {
         agent = GetComponent<NavMeshAgent>(); //Adiciona a referencia para o componente NavMeshAgent.
+        rBody = GetComponent<Rigidbody>();
         NavMeshAgentReconfig();
         grounded = false;
+
+        stoppingDistance = agent.stoppingDistance * transform.localScale.x; //Um cálculo simples para regular a distancia de parada da aranha dependendo da sua escala.
+        agent.stoppingDistance = stoppingDistance;
     }
 
     // Update is called once per frame
@@ -30,8 +37,9 @@ public class EnemyStats : MonoBehaviour {
         grounded = groundChk.isGrounded();
 
         agent.enabled = grounded; //Se ele ta no chão, ativa o agente.
+        rBody.isKinematic = grounded; //Ativa o isKinematic do componente RigidBody.
 
-        //Debug.DrawLine(transform.localPosition, -Vector3.up, Color.black);
+        //Debug.DrawLine(transform.localPosition, -Vector3.up, Color.black);        
     }
 
     private void NavMeshAgentReconfig() //Essa função irá substituir alguns valores no componente NavMeshAgent de acordo com os valores presentes nessa classe.
---
+++ Assets/Scripts/Enemy/SpiderNavMeshAnim.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderNavMeshAnim : MonoBehaviour {
+
+    private Animator anim; //Referencia ao componente de animação.
+    private NavMeshAgent agent; //Referencia ao componente NavMeshAgent.
+    private EnemyStats enemyStats; //Referencia ao componente EnemyStats.
+
+    private bool moving;
+
+    private float speedBlend;
+    private float speed;
+
+    private float enemyHealth;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        enemyStats = GetComponent<EnemyStats>();
+
+        speed = enemyStats.speed;
+        enemyHealth = enemyStats.health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (agent.enabled) //Todo o comportamento só pode ser iniciado se o component NavMeshAgent estiver enable, para evitar erros.
+        {
+
+            if (agent.velocity != Vector3.zero) //Se a velocidade dele for diferente de zero.
+            {
+                moving = true;
+                speedBlend = agent.velocity.magnitude / speed; //Pequeno cálculo para setar o blend das animações de correr e andar.
+            }
+            else
+            {
+                moving = false;
+                speedBlend = 0;
+            }
+            
+            if (agent.remainingDistance - agent.stoppingDistance < 0) //Se a distancia restante menos a distancia de parada for negativa, ataque.
+            {
+                anim.SetTrigger("Attacking"); //Ativa animação de ataque.
+            }
+
+            if (enemyHealth >= 0)//Se a vida do inimigo for menor ou igual a 0, chama a animação de morrer.
+            {
+                anim.SetTrigger("Dead"); 
+            }
+
+            anim.SetBool("Moving", moving); //Seta a variável Moving no Animator desse componente para o valor de moving.
+            anim.SetFloat("Speed", speedBlend);//Seta a variável Speed no Animator desse componente para o valor de speedBlend.
+        }
+
+	}
+}
